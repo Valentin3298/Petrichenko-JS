79ddfb5a88debf02f60473d65409e2080dc035a7
Valentin <zloy.ax@gmail.com>
2020-09-17 20:22:09 +0300

    filmsDB

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 "use strict";
 
+const number0Films = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMuvieDB = {
+  //DB Data Base база данных
+  count: number0Films,
+  muvies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных филбмов", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMuvieDB.muvies[a] = b;
+personalMuvieDB.muvies[c] = d;
+
+console.log(personalMuvieDB);
